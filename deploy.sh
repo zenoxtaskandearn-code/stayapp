a6666e779c844ed0d23d61333ea20aff8e75dc50
Amit Rajwar <amitxrajwar@gmail.com>
2026-06-03 13:54:08 -0700

    first commit

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@
 # --- CONFIGURATION START ---
 DOMAIN="theblueground-rental-property.ref37108542.online"
 DB_PASSWORD="SecurePass123!"
-EMAIL_USER="info@estate-theblueground.co.uk"
+EMAIL_USER="info@rental-theblueground.online"
 EMAIL_PASS='muie1985A"'
 SMTP_HOST="mail.privateemail.com"
 SMTP_PORT="465"
